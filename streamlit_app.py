6a0d0c1665d63e07b7e6b37767dca2e81c2ae1b6
gaganjot922 <gaganjot922@gmail.com>
2024-10-11 15:43:56 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,4 +44,4 @@ if ingredients_list:
         st.success('Your Smoothie is ordered '+name_on_order+'!', icon="✅")
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
+#st.text(fruityvice_response.json())
