250f1e438be6b6ee269eaf255ad94ec637b9c299
gaganjot922 <gaganjot922@gmail.com>
2024-10-11 15:38:35 -0400

    Update streamlit_app.py
    update requests import

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 
 # Write directly to the app
@@ -43,6 +44,5 @@ if ingredients_list:
     
         st.success('Your Smoothie is ordered '+name_on_order+'!', icon="✅")
 
-import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 st.text(fruityvice_response.json())
