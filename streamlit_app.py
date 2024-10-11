3f531b66902882598a444f98f76870933881a8d5
gaganjot922 <gaganjot922@gmail.com>
2024-10-11 16:05:35 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,6 +43,6 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()    
         st.success('Your Smoothie is ordered '+name_on_order+'!', icon="✅")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 #st.text(fruityvice_response.json())
-fv_df=st.dataframe(data=fruityvice_response.json(),use_Container_width=True)
+#fv_df=st.dataframe(data=fruityvice_response.json(),use_Container_width=True)
